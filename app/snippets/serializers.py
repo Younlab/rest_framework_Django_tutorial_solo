aa64f93be96eaeef6a49eba5435437b441d7a116
Sehyeon Youn <d.sehyeon@gmail.com>
2018-07-22 20:04:07 +0900

    serializers.py end 이후 새로 ModelForm 처럼 사용하기로 재정의

--- app/snippets/serializers.py
+++ app/snippets/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import LANGUAGE_CHOICES, STYLE_CHOICES
+from .models import LANGUAGE_CHOICES, STYLE_CHOICES, Snippet
 
 
 class SnippetSerializer(serializers.Serializer):
@@ -11,3 +11,14 @@ class SnippetSerializer(serializers.Serializer):
     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
     style = serializers.ChoiceField(choices=STYLE_CHOICES, default='freiendly')
 
+    def create(self, validated_data):
+        return Snippet.objects.create(**validated_data)
+
+    def update(self, instance , validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.code = validated_data.get('code', instance.code)
+        instance.linenos = validated_data.get('linenos', instance.code)
+        instance.language = validated_data.get('language', instance.language)
+        instance.style = validated_data.get('style', instance.style)
+        instance.save()
+        return instance
