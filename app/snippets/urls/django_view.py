e96039b3381f87d2e591a7c9b581bc25fb64cddf
Sehyeon Youn <d.sehyeon@gmail.com>
2018-07-25 13:41:08 +0900

    generics cbv

--- app/snippets/urls/django_view.py
+++ app/snippets/urls/django_view.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from ..views import django_view as views
+from ..views import mixins as views
 
 urlpatterns = [
-    path('snippets/', views.snippet_list, name='snippet-list'),
-    path('snippets/<int:pk>/', views.snippet_detail, name='snippet-detail'),
+    path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
 
 ]
